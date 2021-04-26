4a1eaa645ad959ce6bde47a97f558ef0e8cb98bf
Niko Pasanen <niko@pasanen.me>
2021-04-26 20:15:18 +0300

    allow all filetypes in usage.py +blackify

--- usage.py
+++ usage.py
@@ -15,11 +15,11 @@ du.configure_upload(app, UPLOAD_FOLDER_ROOT)
 def get_upload_component(id):
     return du.Upload(
         id=id,
-        text='Drag and Drop files here',
-        text_completed='Completed: ',
+        text="Drag and Drop files here",
+        text_completed="Completed: ",
         cancel_button=True,
         max_file_size=1800,  # 1800 Mb
-        filetypes=['csv', 'zip'],
+        # filetypes=['csv', 'zip'],
         upload_id=uuid.uuid1(),  # Unique session id
         max_files=10,
     )
@@ -29,21 +29,22 @@ def get_app_layout():
 
     return html.Div(
         [
-            html.H1('Demo'),
+            html.H1("Demo"),
             html.Div(
                 [
-                    get_upload_component(id='dash-uploader'),
-                    html.Div(id='callback-output'),
+                    get_upload_component(id="dash-uploader"),
+                    html.Div(id="callback-output"),
                 ],
                 style={  # wrapper div style
-                    'textAlign': 'center',
-                    'width': '600px',
-                    'padding': '10px',
-                    'display': 'inline-block'
-                }),
+                    "textAlign": "center",
+                    "width": "600px",
+                    "padding": "10px",
+                    "display": "inline-block",
+                },
+            ),
         ],
         style={
-            'textAlign': 'center',
+            "textAlign": "center",
         },
     )
 
@@ -63,17 +64,16 @@ app.layout = get_app_layout
 
 
 @app.callback(
-    Output('callback-output', 'children'),
-    [Input('dash-uploader', 'uploadedFiles')],
+    Output("callback-output", "children"),
+    [Input("dash-uploader", "uploadedFiles")],
     [
-        State('dash-uploader', 'fileNames'),
-        State('dash-uploader', 'upload_id'),
-        State('dash-uploader', 'isCompleted'),
-        State('dash-uploader', 'newestUploadedFileName')
+        State("dash-uploader", "fileNames"),
+        State("dash-uploader", "upload_id"),
+        State("dash-uploader", "isCompleted"),
+        State("dash-uploader", "newestUploadedFileName"),
     ],
 )
-def callback_on_completion(n_files, filenames, upload_id, iscompleted,
-                           latest_file):
+def callback_on_completion(n_files, filenames, upload_id, iscompleted, latest_file):
 
     if n_files == 0:
         return  # no files uploaded yet.
@@ -95,5 +95,5 @@ def callback_on_completion(n_files, filenames, upload_id, iscompleted,
     return html.Div("No Files Uploaded Yet!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
