daa1aa6390ada5be19f5928608c95b5f44c3c580
Niko Pasanen <niko@pasanen.me>
2021-05-04 17:26:16 +0300

    blackify

--- devscripts/post_build.py
+++ devscripts/post_build.py
@@ -3,17 +3,18 @@ import shutil
 
 root = Path(__file__).resolve().parent.parent
 
-components = root / 'src' / 'lib' / 'components'
+components = root / "src" / "lib" / "components"
 
-component_names = [x.name[:-9] for x in components.glob('*.react.js')]
+component_names = [x.name[:-9] for x in components.glob("*.react.js")]
 
-folder_from = root / 'dash_uploader'
-folder_to = folder_from / '_build'
+folder_from = root / "dash_uploader"
+folder_to = folder_from / "_build"
 
-filenames = ['metadata.json', 'package-info.json', '_imports_.py'
-             ] + [x + '.py' for x in component_names]
+filenames = ["metadata.json", "package-info.json", "_imports_.py"] + [
+    x + ".py" for x in component_names
+]
 
 for filename in filenames:
     shutil.move(folder_from / filename, folder_to / filename)
 
-Path(folder_to / '__init__.py').touch(exist_ok=True)
+Path(folder_to / "__init__.py").touch(exist_ok=True)
