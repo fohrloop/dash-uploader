1fbd68fb8cee8f72dd4cd78309e38ccfe1e0eb81
hongfupan <panhongfu@dp.tech>
2023-10-23 16:14:49 +0800

    optimize setup package to support local dev in M2 architecture

--- dash_uploader/utils.py
+++ dash_uploader/utils.py
@@ -17,8 +17,6 @@ def dash_version_is_at_least(req_version="1.12"):
     the argument "req_version".
     This is a private method, and should not be exposed to users.
     """
-    if isinstance(dash_version, version.LegacyVersion):
-        return False
     return dash_version >= version.parse(req_version)
 
 
